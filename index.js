5d447ce544b377e0b75e06b333338d7132444082
Alfred Ramdayal <33667790+Alfred4U2NV@users.noreply.github.com>
2017-12-04 13:50:46 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+//Below will be a function for subtraction
+function subtract(num1, num2){
+  return num1-num2;
+}
+//Below will be a function for multiply
+function multiply(num1, num2){
+  return num1*num2;
+}
+ //Below will be a function for divide
+function divide(num1, num2){
+  return num1/num2;
+}
+
+function toCelcius(fahrenheit){
+  return fahrenheit -32 * 5/9
+}
+
+//F - 32 *5/9
